77b3fe706a3781f90778091e567fc1c27a836037
lynxBios <123711868+lynxBios@users.noreply.github.com>
2023-11-14 16:44:53 +0300

    fix: cdk bucket creation logic

--- cdk-task-2/bin/cdk-task-2.ts
+++ cdk-task-2/bin/cdk-task-2.ts
@@ -1,21 +1,50 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
+import * as cloudfrontOrigins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 import { CdkTask2Stack } from '../lib/cdk-task-2-stack';
 
 const app = new cdk.App();
-new CdkTask2Stack(app, 'CdkTask2Stack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+const stack = new CdkTask2Stack(app, 'CdkTask2Stack', {
+    env: { account: '730043614514', region: 'eu-central-1' },  
+});
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const awsBucket = new s3.Bucket(stack, 'BucketFromCDK', {
+    bucketName: 'bucket-from-cdk',
+    websiteIndexDocument: 'index.html',
+    publicReadAccess: false,
+    blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+    removalPolicy: cdk.RemovalPolicy.DESTROY
+});
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+const originAccessIdentity = new cloudfront.OriginAccessIdentity(stack, 'BucketFromCDK', {
+  comment: awsBucket.bucketName
+});
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+const cloudFront = new cloudfront.Distribution(stack, 'Distribution', {
+    defaultBehavior: {
+        origin: new cloudfrontOrigins.S3Origin(awsBucket, {
+          originAccessIdentity         
+          }
+        ),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+    },
+    defaultRootObject: 'index.html',
+});
+
+awsBucket.grantRead(originAccessIdentity)
+
+new s3deploy.BucketDeployment(stack, 'BucketFromCDKDeployment', {
+  sources: [s3deploy.Source.asset('./dist')],
+  destinationBucket: awsBucket,
+  distribution: cloudFront,
+  distributionPaths: ['/*']
+});
+new cdk.CfnOutput(stack, 'Domain URL', {
+  value: cloudFront.distributionDomainName,
+});
+new cdk.CfnOutput(stack, 'S3 URL', {
+value: awsBucket.bucketDomainName,
 });
