d0f88c8d4d6489ae7f24b72559a87ada880cba1f
lynxBios <123711868+lynxBios@users.noreply.github.com>
2023-11-15 18:36:59 +0300

    fix: s3bucket creation

--- cdk-task-2/bin/cdk-task-2.ts
+++ cdk-task-2/bin/cdk-task-2.ts
@@ -1,50 +1,13 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import * as cloudfrontOrigins from 'aws-cdk-lib/aws-cloudfront-origins';
-import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
-import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
-import { CdkTask2Stack } from '../lib/cdk-task-2-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { CdkStack } from "../lib/cdk-task-2-stack";
 
 const app = new cdk.App();
-const stack = new CdkTask2Stack(app, 'CdkTask2Stack', {
-    env: { account: '730043614514', region: 'eu-central-1' },  
-});
 
-const awsBucket = new s3.Bucket(stack, 'BucketFromCDK', {
-    bucketName: 'bucket-from-cdk',
-    websiteIndexDocument: 'index.html',
-    publicReadAccess: false,
-    blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-    removalPolicy: cdk.RemovalPolicy.DESTROY
-});
-
-const originAccessIdentity = new cloudfront.OriginAccessIdentity(stack, 'BucketFromCDK', {
-  comment: awsBucket.bucketName
-});
-
-const cloudFront = new cloudfront.Distribution(stack, 'Distribution', {
-    defaultBehavior: {
-        origin: new cloudfrontOrigins.S3Origin(awsBucket, {
-          originAccessIdentity         
-          }
-        ),
-        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-    },
-    defaultRootObject: 'index.html',
-});
-
-awsBucket.grantRead(originAccessIdentity)
-
-new s3deploy.BucketDeployment(stack, 'BucketFromCDKDeployment', {
-  sources: [s3deploy.Source.asset('./dist')],
-  destinationBucket: awsBucket,
-  distribution: cloudFront,
-  distributionPaths: ['/*']
-});
-new cdk.CfnOutput(stack, 'Domain URL', {
-  value: cloudFront.distributionDomainName,
-});
-new cdk.CfnOutput(stack, 'S3 URL', {
-value: awsBucket.bucketDomainName,
+new CdkStack(app, "CdkStack", {
+  env: {
+    account: '730043614514',
+    region: 'eu-central-1'
+  }
 });
--- cdk-task-2/lib/cdk-task-2-stack.ts
+++ cdk-task-2/lib/cdk-task-2-stack.ts
@@ -1,16 +1,60 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+// aws
+import * as cdk from "aws-cdk-lib";
+import { CloudFrontWebDistribution, OriginAccessIdentity } from "aws-cdk-lib/aws-cloudfront";
+import { AccountRootPrincipal, PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { BlockPublicAccess, Bucket } from "aws-cdk-lib/aws-s3";
+import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
+import { Construct } from "constructs";
 
-export class CdkTask2Stack extends cdk.Stack {
+export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new Bucket(this, "Bucket", {
+      versioned: true,
+      bucketName: "bucket-from-cdk",
+      websiteIndexDocument: "index.html",      
+      autoDeleteObjects: true,
+      publicReadAccess: false,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkTask2Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const oai = new OriginAccessIdentity(this, "OriginAccessIdentity", {
+      comment: "Cloudfront OAI"
+    });
+
+    bucket.grantRead(oai);
+
+    bucket.addToResourcePolicy(
+      new PolicyStatement({
+        actions: ["s3:GetObject"],
+        resources: [bucket.arnForObjects("*")],
+        principals: [new AccountRootPrincipal()]
+      })
+    );
+
+    const distribution = new CloudFrontWebDistribution(this, "CloudFrontWebDistribution", {
+        originConfigs: [
+          { 
+            s3OriginSource: {
+              s3BucketSource: bucket,
+              originAccessIdentity: oai
+            },
+            behaviors: [
+              {
+                isDefaultBehavior: true
+              }
+            ]            
+          }
+        ]
+      });
+
+    new BucketDeployment(this, "BucketDeployment", {
+      sources: [Source.asset("../dist")],
+      destinationBucket: bucket,
+      distribution: distribution,
+      distributionPaths: ["/*"]
+    });
   }
 }
