7ccbe0c6645fd4c68764bc6b0676866958b6fd3a
lynxBios <123711868+lynxBios@users.noreply.github.com>
2023-11-20 01:39:28 +0300

    feat: products service implemented

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  product: "https://12zkhh481m.execute-api.eu-central-1.amazonaws.com/products",
+  product: "https://12zkhh481m.execute-api.eu-central-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
