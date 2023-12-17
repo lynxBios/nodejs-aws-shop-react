bc6e50e84b64424b77a169e1c3b6360afe956cea
lynxBios <123711868+lynxBios@users.noreply.github.com>
2023-12-18 01:02:43 +0300

    feat: update apiPath.ts

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://12zkhh481m.execute-api.eu-central-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://12zkhh481m.execute-api.eu-central-1.amazonaws.com",
+  import: "https://hn9h7jf7x2.execute-api.eu-central-1.amazonaws.com",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
